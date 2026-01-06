fe36e4838ae443c81a6140c4b56bb7f666f23e52
Charlotte <18631656747@163.com>
2026-01-06 10:43:20 +0800

    Initial Commit

---
+++ Weli/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weli
+//
+//  Created by Charlotte on 2026/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
